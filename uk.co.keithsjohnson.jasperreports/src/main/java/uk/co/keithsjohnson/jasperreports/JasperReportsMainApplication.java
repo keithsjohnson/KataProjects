9880289d062f960b5c6ab3acf573a7ed245266d9
keithsjohnson <keithsjohnson@yahoo.com>
2016-01-27 22:58:03 +0000

    Initial Version

---
+++ uk.co.keithsjohnson.jasperreports/src/main/java/uk/co/keithsjohnson/jasperreports/JasperReportsMainApplication.java
@@ -0,0 +1,8 @@
+package uk.co.keithsjohnson.jasperreports;
+
+public class JasperReportsMainApplication {
+
+	public static void main(String[] args) {
+		System.out.println("Hi from JasperReportsMainApplication");
+	}
+}
