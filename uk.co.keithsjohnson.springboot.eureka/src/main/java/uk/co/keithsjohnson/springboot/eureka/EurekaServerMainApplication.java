8d7f6df4a590d8bf9d1a6a2d5cd19cbb02fb8e2f
keithsjohnson <keithsjohnson@yahoo.com>
2016-02-15 22:52:36 +0000

    Add Eureka Server

--- uk.co.keithsjohnson.springboot.eureka/src/main/java/uk/co/keithsjohnson/springboot/eureka/EurekaServerMainApplication.java
+++ uk.co.keithsjohnson.springboot.eureka/src/main/java/uk/co/keithsjohnson/springboot/eureka/EurekaServerMainApplication.java
@@ -2,8 +2,10 @@ package uk.co.keithsjohnson.springboot.eureka;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 import org.springframework.context.ConfigurableApplicationContext;
 
+@EnableEurekaServer
 @SpringBootApplication
 public class EurekaServerMainApplication {
 
